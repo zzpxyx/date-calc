e0513b79dc50f09a049e4ef4b79edc1b6e6f5e83
zzpxyx <zptomz@gmail.com>
2017-07-18 22:28:44 -0500

    Add functions for calculating dates. The demo page is kind of working.

--- src/date-calc.js
+++ src/date-calc.js
@@ -1,4 +1,39 @@
 /**
+ * A class for calculating dates.
+ */
+var DateCalc = (function () {
+    function DateCalc() {
+    }
+    /**
+     * Calculate the number of days between the start and the end dates.
+     * @param startDateString The string for the start date.
+     * @param endDateString The string for the end date.
+     * @return The number of days in between.
+     * For example, given 2017-01-01 and 2017-01-02, the return value will be 1.
+     */
+    DateCalc.daysBetween = function (startDateString, endDateString) {
+        var startDate = LocalDate.parse(startDateString);
+        var endDate = LocalDate.parse(endDateString);
+        var period = new Period(startDate, endDate);
+        return period.getDays();
+    };
+    /**
+     * Calculate the period between the start and the end dates.
+     * @param startDateString The string for the start date.
+     * @param endDateString The string for the end date.
+     * @return The string describing the period.
+     * For example, given 2017-01-01 and 2018-03-04, the return value will be
+     * 1 year 2 months 3days.
+     */
+    DateCalc.periodBetween = function (startDateString, endDateString) {
+        var startDate = LocalDate.parse(startDateString);
+        var endDate = LocalDate.parse(endDateString);
+        var period = new Period(startDate, endDate);
+        return period.toString();
+    };
+    return DateCalc;
+}());
+/**
  * A class to represent a date without the time zone.
  */
 var LocalDate = (function () {
@@ -32,10 +67,23 @@ var LocalDate = (function () {
     };
     /**
      * Check if the given year is a leap year.
+     * @param year The given year.
      * @return True if the year is a leap year; false otherwise.
      */
     LocalDate.isLeapYear = function (year) {
         return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+    };
+    /**
+     * Construct a LocalDate object by parsing a date string.
+     * @param dateString The given date string.
+     * @return The constructed LocalDate object.
+     */
+    LocalDate.parse = function (dateString) {
+        var date = new Date(dateString);
+        var year = date.getUTCFullYear();
+        var month = date.getUTCMonth() + 1;
+        var day = date.getUTCDate();
+        return new LocalDate(year, month, day);
     };
     return LocalDate;
 }());
@@ -53,6 +101,44 @@ var Period = (function () {
      */
     Period.prototype.getDays = function () {
         return this.endDate.sinceDayZero() - this.startDate.sinceDayZero();
+    };
+    /**
+     * Get the string representation of the Period object.
+     * @return The string representation.
+     */
+    Period.prototype.toString = function () {
+        var years = this.endDate.year - this.startDate.year;
+        var months = this.endDate.month - this.startDate.month;
+        var days = this.endDate.day - this.startDate.day;
+        var daysInMonth = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+        // Normalize the period.
+        if (days < 0) {
+            months--;
+            days += daysInMonth[this.endDate.month];
+            if (LocalDate.isLeapYear(this.endDate.year)) {
+                days++;
+            }
+        }
+        if (months < 0) {
+            years--;
+            months += 12;
+        }
+        // Format the return string.
+        var ret = "";
+        var values = [years, months, days];
+        var units = ["year", "month", "day"];
+        for (var i = 0; i < 3; i++) {
+            if (values[i] > 0) {
+                if (ret !== "") {
+                    ret += " ";
+                }
+                ret += values[i] + " " + units[i];
+                if (values[i] > 1) {
+                    ret += "s";
+                }
+            }
+        }
+        return ret;
     };
     return Period;
 }());
--- src/date-calc.ts
+++ src/date-calc.ts
@@ -1,4 +1,40 @@
 /**
+ * A class for calculating dates.
+ */
+class DateCalc {
+    /**
+     * Calculate the number of days between the start and the end dates.
+     * @param startDateString The string for the start date.
+     * @param endDateString The string for the end date.
+     * @return The number of days in between.
+     * For example, given 2017-01-01 and 2017-01-02, the return value will be 1.
+     */
+    static daysBetween(startDateString: string, endDateString: string): number {
+        let startDate: LocalDate = LocalDate.parse(startDateString);
+        let endDate: LocalDate = LocalDate.parse(endDateString);
+        let period: Period = new Period(startDate, endDate);
+        return period.getDays();
+    }
+
+    /**
+     * Calculate the period between the start and the end dates.
+     * @param startDateString The string for the start date.
+     * @param endDateString The string for the end date.
+     * @return The string describing the period.
+     * For example, given 2017-01-01 and 2018-03-04, the return value will be
+     * 1 year 2 months 3days.
+     */
+    static periodBetween(
+        startDateString: string,
+        endDateString: string): string {
+        let startDate: LocalDate = LocalDate.parse(startDateString);
+        let endDate: LocalDate = LocalDate.parse(endDateString);
+        let period: Period = new Period(startDate, endDate);
+        return period.toString();
+    }
+}
+
+/**
  * A class to represent a date without the time zone.
  */
 class LocalDate {
@@ -38,10 +74,24 @@ class LocalDate {
 
     /**
      * Check if the given year is a leap year.
+     * @param year The given year.
      * @return True if the year is a leap year; false otherwise.
      */
     static isLeapYear(year: number): boolean {
         return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+    }
+
+    /**
+     * Construct a LocalDate object by parsing a date string.
+     * @param dateString The given date string.
+     * @return The constructed LocalDate object.
+     */
+    static parse(dateString: string): LocalDate {
+        let date: Date = new Date(dateString);
+        let year: number = date.getUTCFullYear();
+        let month: number = date.getUTCMonth() + 1;
+        let day: number = date.getUTCDate();
+        return new LocalDate(year, month, day);
     }
 }
 
@@ -60,5 +110,48 @@ class Period {
      */
     getDays(): number {
         return this.endDate.sinceDayZero() - this.startDate.sinceDayZero();
+    }
+
+    /**
+     * Get the string representation of the Period object.
+     * @return The string representation.
+     */
+    toString(): string {
+        let years: number = this.endDate.year - this.startDate.year;
+        let months: number = this.endDate.month - this.startDate.month;
+        let days: number = this.endDate.day - this.startDate.day;
+        let daysInMonth: number[] =
+            [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+        // Normalize the period.
+        if (days < 0) {
+            months--;
+            days += daysInMonth[this.endDate.month];
+            if (LocalDate.isLeapYear(this.endDate.year)) {
+                days++;
+            }
+        }
+        if (months < 0) {
+            years--;
+            months += 12;
+        }
+
+        // Format the return string.
+        let ret: string = "";
+        let values: number[] = [years, months, days];
+        let units: string[] = ["year", "month", "day"];
+        for (let i: number = 0; i < 3; i++) {
+            if (values[i] > 0) {
+                if (ret !== "") {
+                    ret += " ";
+                }
+                ret += values[i] + " " + units[i];
+                if (values[i] > 1) {
+                    ret += "s";
+                }
+            }
+        }
+
+        return ret;
     }
 }
