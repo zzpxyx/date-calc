199915ec5e5ee8f978e0de814e1a4045db910496
zzpxyx <zptomz@gmail.com>
2018-09-19 23:49:29 -0500

    Start major revision.
    The goal is to support calculating the date given a date and a period.

--- src/date-calc.ts
+++ src/date-calc.ts
@@ -8,10 +8,37 @@ class DateCalc {
      * @param dateString2 A string representing another date.
      * @return A Period object for the period between the two dates.
      */
-    static between(dateString1: string, dateString2: string): Period {
-        let date1: LocalDate = LocalDate.parse(dateString1);
-        let date2: LocalDate = LocalDate.parse(dateString2);
-        return new Period(date1, date2);
+    public static calculate(input1: string, input2: string): string {
+        let localDate1: LocalDate;
+        let localDate2: LocalDate;
+        let validationScore: number = 0;
+        localDate1 = LocalDate.parse(input1);
+        localDate2 = LocalDate.parse(input2);
+        validationScore += localDate1 ? 1 : 0;
+        validationScore += localDate2 ? 2 : 0;
+        switch (validationScore) {
+            case 3:
+                let days = Math.abs(localDate1.daysUntil(localDate2));
+                let daysString = days + " " + Utils.pluralize(days, "day");
+                let periodString = Period.between(localDate1, localDate2)
+                    .toString();
+                return daysString === periodString ?
+                    daysString : daysString + " (" + periodString + ")";
+            //case [true, false]:
+            //    if (Period.tryParse(input2, period)) {
+            //        return localDate1.addPeriod(period).toString();
+            //    } else {
+            //        throw new Error("Second input is invalid.");
+            //    }
+            //case [false, true]:
+            //    if (Period.tryParse(input1, period)) {
+            //        return localDate2.addPeriod(period).toString();
+            //    } else {
+            //        throw new Error("First input is invalid.");
+            //    }
+            //case [false, false]:
+            //    throw new Error("Either or both inputs are invalid.");
+        }
     }
 }
 
@@ -19,22 +46,10 @@ class DateCalc {
  * A class to represent a date without the time zone.
  */
 class LocalDate {
-    constructor(
+    private constructor(
         readonly year: number,
         readonly month: number,
         readonly day: number) {
-        if (!LocalDate.validateDate(year, month, day)) {
-            throw new Error("Please check date format.");
-        }
-    }
-
-    /**
-     * Check if the given year is a leap year.
-     * @param year The given year.
-     * @return True if the year is a leap year; false otherwise.
-     */
-    static isLeapYear(year: number): boolean {
-        return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
     }
 
     /**
@@ -42,46 +57,92 @@ class LocalDate {
      * @param dateString The given date string.
      * @return The constructed LocalDate object.
      */
-    static parse(dateString: string): LocalDate {
-        let localDate: LocalDate;
-        let date: Date;
+    public static parse(dateString: string): LocalDate {
         let year: number = 0;
         let month: number = 0;
         let day: number = 0;
         if (dateString.toLowerCase() === "today") {
-            date = new Date();
+            let date: Date = new Date();
             year = date.getFullYear();
             month = date.getMonth() + 1;
             day = date.getDate();
         } else {
             let datePieces: string[];
             if (dateString.indexOf("-") > 0) {
-                // Parse date like 2017-01-20.
+                // Parse date strings like 2017-01-20.
                 datePieces = /^(\d{1,4})-(\d{1,2})-(\d{1,2})$/.exec(dateString);
-                if (datePieces != null) {
+                if (datePieces) {
                     year = +datePieces[1];
                     month = +datePieces[2];
                     day = +datePieces[3];
                 }
             } else if (dateString.indexOf("/") > 0) {
-                // Parse date like 01/20/2017.
+                // Parse date strings like 01/20/2017.
                 datePieces = /^(\d{1,2})\/(\d{1,2})\/(\d{1,4})$/.exec(dateString);
-                if (datePieces != null) {
+                if (datePieces) {
                     year = +datePieces[3];
                     month = +datePieces[1];
                     day = +datePieces[2];
                 }
             } else if (dateString.indexOf(" ") > 0) {
-                // Parse date like Jan 20 2017 or January 20 2017.
+                // Parse date strings like Jan 20 2017 or January 20 2017.
                 datePieces = /^(.+)\s(\d{1,2})\s(\d{1,4})$/.exec(dateString);
-                if (datePieces != null) {
+                if (datePieces) {
                     year = +datePieces[3];
                     month = LocalDate.monthStringToNumber(datePieces[1]);
                     day = +datePieces[2];
                 }
             }
         }
-        return new LocalDate(year, month, day);
+        if (LocalDate.validateDate(year, month, day)) {
+            return new LocalDate(year, month, day);
+        } else {
+            throw new Error("Invalid date or unsupported format.");
+        }
+    }
+
+    public daysUntil(endDate: LocalDate): number {
+        return endDate.sinceDayZero() - this.sinceDayZero();
+    }
+
+    /**
+     * Count the number of days since the hypothetical Day 0.
+     * Day 0 is the day before 0001-1-1.
+     * @return The number of days since Day 0.
+     * For example, 0001-1-1 will have the return value of 1.
+     */
+    public sinceDayZero(): number {
+        let total: number = 0;
+
+        // Count the number of days to the end of the previous year.
+        let prevYear: number = this.year - 1;
+        total += 365 * prevYear;
+        total += Math.floor(prevYear / 4);
+        total -= Math.floor(prevYear / 100);
+        total += Math.floor(prevYear / 400);
+
+        // Count the number of days in the given year.
+        let daysToEndOfPrevMonth: number[] =
+            [0, 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
+        total += daysToEndOfPrevMonth[this.month];
+        if ((this.month > 2) && Utils.isLeapYear(this.year)) {
+            total++;
+        }
+        total += this.day;
+
+        return total;
+    }
+
+    /**
+     * Get the string representation of the LocalDate object.
+     * Note that the returned string will have the following format:
+     *   yyyy-mm-dd
+     * @return The string representation.
+     */
+    public toString(): string {
+        return LocalDate.padZero(this.year, 4) + "-" +
+            LocalDate.padZero(this.month, 2) + "-" +
+            LocalDate.padZero(this.day, 2);
     }
 
     /**
@@ -90,7 +151,7 @@ class LocalDate {
      * @param length The given length.
      * @return A string containing the given value with padded leading zeroes.
      */
-    static padZero(value: number, length: number): string {
+    private static padZero(value: number, length: number): string {
         let paddedValue: string = "" + value;
         while (paddedValue.length < length) {
             paddedValue = "0" + paddedValue;
@@ -105,10 +166,14 @@ class LocalDate {
      * @param day The day in the given date.
      * @return True if valid; false, otherwise.
      */
-    static validateDate(year: number, month: number, day: number): boolean {
+    private static validateDate(
+        year: number,
+        month: number,
+        day: number
+    ): boolean {
         let daysInMonth: number[] =
             [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-        if (LocalDate.isLeapYear(year)) {
+        if (Utils.isLeapYear(year)) {
             daysInMonth[2] = 29;
         }
         if (!isFinite(year) || year < 1 ||
@@ -124,7 +189,7 @@ class LocalDate {
      * @param monthString The month name or abbreviation.
      * @return The month number. -1 if monthString is not valid.
      */
-    static monthStringToNumber(monthString: string): number {
+    private static monthStringToNumber(monthString: string): number {
         let month: number = -1;
 
         // Remove trailing period for month abbreviation.
@@ -149,92 +214,33 @@ class LocalDate {
         }
         return month;
     }
-
-    /**
-     * Count the number of days since the hypothetical Day 0.
-     * Day 0 is the day before 0001-1-1.
-     * @return The number of days since Day 0.
-     * For example, 0001-1-1 will have the return value of 1.
-     */
-    sinceDayZero(): number {
-        let total: number = 0;
-
-        // Count the number of days to the end of the previous year.
-        let prevYear: number = this.year - 1;
-        total += 365 * prevYear;
-        total += Math.floor(prevYear / 4);
-        total -= Math.floor(prevYear / 100);
-        total += Math.floor(prevYear / 400);
-
-        // Count the number of days in the given year.
-        let daysToEndOfPrevMonth: number[] =
-            [0, 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
-        total += daysToEndOfPrevMonth[this.month];
-        if ((this.month > 2) && LocalDate.isLeapYear(this.year)) {
-            total++;
-        }
-        total += this.day;
-
-        return total;
-    }
-
-    /**
-     * Get the string representation of the LocalDate object.
-     * Note that the returned string will have the following format:
-     *   yyyy-mm-dd
-     * @return The string representation.
-     */
-    toString(): string {
-        return LocalDate.padZero(this.year, 4) + "-" +
-            LocalDate.padZero(this.month, 2) + "-" +
-            LocalDate.padZero(this.day, 2);
-    }
 }
 
 /**
  * A class to represent an amount of date-based time.
  */
 class Period {
-    /**
-     * The two dates will be swapped if the start date is later than the end date.
-     */
-    constructor(
-        readonly startDate: LocalDate,
-        readonly endDate: LocalDate) {
-        if (startDate == null || endDate == null) {
-            throw new Error("Please check date format.");
-        }
-        if (startDate.sinceDayZero() > endDate.sinceDayZero()) {
-            let tmpDate: LocalDate = this.startDate;
-            this.startDate = this.endDate;
-            this.endDate = tmpDate;
-        }
+    private constructor(
+        readonly years: number,
+        readonly months: number,
+        readonly days: number) {
     }
 
-    /**
-     * Calculate the number of days between the start and the end dates.
-     * @return The number of days in between.
-     */
-    getDays(): number {
-        return this.endDate.sinceDayZero() - this.startDate.sinceDayZero();
-    }
-
-    /**
-     * Calculate the years, months, and days between the start and end dates.
-     * @return An array with the number of years, months, and days.
-     */
-    getYearsMonthsDays(): number[] {
-        let years: number = this.endDate.year - this.startDate.year;
-        let months: number = this.endDate.month - this.startDate.month;
-        let days: number = this.endDate.day - this.startDate.day;
+    public static between(localDate1: LocalDate, localDate2: LocalDate): Period {
+        if (localDate1.daysUntil(localDate2) < 0) {
+            [localDate1, localDate2] = [localDate2, localDate1];
+        }
+        let years: number = localDate2.year - localDate1.year;
+        let months: number = localDate2.month - localDate1.month;
+        let days: number = localDate2.day - localDate1.day;
         let daysInMonth: number[] =
             [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
         // Normalize the period.
         if (days < 0) {
             months--;
-            days += daysInMonth[this.endDate.month];
-            if (LocalDate.isLeapYear(this.endDate.year)) {
+            days += daysInMonth[localDate2.month];
+            if (Utils.isLeapYear(localDate2.year)) {
                 days++;
             }
         }
@@ -243,30 +249,32 @@ class Period {
             months += 12;
         }
 
-        return [years, months, days];
+        return new Period(years, months, days);
     }
 
+    public toString(): string {
+        let components: { value: number, unit: string }[] = [
+            { value: this.years, unit: "year" },
+            { value: this.months, unit: "month" },
+            { value: this.days, unit: "day" }
+        ];
+        return components.filter(c => c.value > 0)
+            .map(c => c.value + " " + Utils.pluralize(c.value, c.unit))
+            .join(" ") || "0 days";
+    }
+}
+
+class Utils {
     /**
-     * Get the string representation of the Period object.
-     * Note that the returned string will have the following format:
-     *   3 years 2 months 1 day
-     * @return The string representation.
+     * Check if the given year is a leap year.
+     * @param year The given year.
+     * @return True if the year is a leap year; false otherwise.
      */
-    toString(): string {
-        let ret: string = "";
-        let values: number[] = this.getYearsMonthsDays();
-        let units: string[] = ["year", "month", "day"];
-        for (let i: number = 0; i < 3; i++) {
-            if (values[i] > 0) {
-                if (ret !== "") {
-                    ret += " ";
-                }
-                ret += values[i] + " " + units[i];
-                if (values[i] > 1) {
-                    ret += "s";
-                }
-            }
-        }
-        return ret;
+    public static isLeapYear(year: number): boolean {
+        return ((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0);
+    }
+
+    public static pluralize(count: number, singular: string): string {
+        return count === 1 ? singular : singular + "s";
     }
 }
