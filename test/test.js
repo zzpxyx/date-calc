b1ce336a6af65c01ffa2221a6d055fe4858c9acf
zzpxyx <zptomz@gmail.com>
2018-09-20 00:14:55 -0500

    Update unit tests.

--- test/test.js
+++ test/test.js
@@ -1,23 +1,17 @@
+describe("DateCalc", function() {
+    it("can calculate the period between the start and the end dates.",
+        function() {
+            expect(DateCalc.calculate("0001-01-01", "0001-01-02"))
+                .toBe("1 day");
+            expect(DateCalc.calculate("2000-01-01", "2001-03-02"))
+                .toBe("426 days (1 year 2 months 1 day)");
+            expect(DateCalc.calculate("today", "Today"))
+                .toBe("0 days");
+        }
+    );
+})
+
 describe("LocalDate", function() {
-    it("can calculate days since hypothetical Day 0.", function() {
-        expect(new LocalDate(1, 1, 1).sinceDayZero()).toBe(1);
-        expect(new LocalDate(1, 1, 2).sinceDayZero()).toBe(2);
-        expect(new LocalDate(1, 2, 2).sinceDayZero()).toBe(33);
-        expect(new LocalDate(1, 3, 3).sinceDayZero()).toBe(62);
-        expect(new LocalDate(4, 3, 3).sinceDayZero()).toBe(1158);
-    });
-    it("can get the string representation.", function() {
-        expect(new LocalDate(1, 1, 1).toString()).toBe("0001-01-01");
-        expect(new LocalDate(2000, 10, 10).toString()).toBe("2000-10-10");
-    });
-    it("can check if a given year is a leap year.", function() {
-        expect(LocalDate.isLeapYear(1)).toBe(false);
-        expect(LocalDate.isLeapYear(4)).toBe(true);
-        expect(LocalDate.isLeapYear(100)).toBe(false);
-        expect(LocalDate.isLeapYear(400)).toBe(true);
-        expect(LocalDate.isLeapYear(1900)).toBe(false);
-        expect(LocalDate.isLeapYear(2000)).toBe(true);
-    });
     it("can parse a string to get a LocalDate object.", function() {
         expect(LocalDate.parse("2017-01-02").toString()).toBe("2017-01-02");
         expect(LocalDate.parse("01/02/2017").toString()).toBe("2017-01-02");
@@ -25,28 +19,18 @@ describe("LocalDate", function() {
         expect(LocalDate.parse("January 2 2017").toString()).toBe("2017-01-02");
         expect(LocalDate.parse("today") instanceof LocalDate).toBe(true);
     });
-    it("can pad zeros according to the required width.", function() {
-        expect(LocalDate.padZero(1, 0)).toBe("1");
-        expect(LocalDate.padZero(1, 1)).toBe("1");
-        expect(LocalDate.padZero(1, 2)).toBe("01");
-        expect(LocalDate.padZero(1, 3)).toBe("001");
-        expect(LocalDate.padZero(1, 4)).toBe("0001");
+    it("can calculate days until another date.", function() {
+        expect(LocalDate.parse("1-1-1").daysUntil(LocalDate.parse("1-1-2")))
+            .toBe(1);
+        expect(LocalDate.parse("1-1-1").daysUntil(LocalDate.parse("1-2-2")))
+            .toBe(32);
+        expect(LocalDate.parse("1-1-1").daysUntil(LocalDate.parse("4-3-3")))
+            .toBe(1157);
     });
-    it("can validate a given date.", function() {
-        expect(LocalDate.validateDate(0, 1, 1)).toBe(false);
-        expect(LocalDate.validateDate(1, 0, 1)).toBe(false);
-        expect(LocalDate.validateDate(1, 1, 0)).toBe(false);
-        expect(LocalDate.validateDate(1, 1, 1)).toBe(true);
-        expect(LocalDate.validateDate(1, 13, 1)).toBe(false);
-        expect(LocalDate.validateDate(1, 2, 29)).toBe(false);
-        expect(LocalDate.validateDate(4, 2, 29)).toBe(true);
-        expect(LocalDate.validateDate("a", 1, 1)).toBe(false);
-    });
-    it("can convert a month name or abbreviation to number.", function() {
-        expect(LocalDate.monthStringToNumber("Jan")).toBe(1);
-        expect(LocalDate.monthStringToNumber("Jan.")).toBe(1);
-        expect(LocalDate.monthStringToNumber("January")).toBe(1);
-        expect(LocalDate.monthStringToNumber("Invalid")).toBe(-1);
+    it("can calculate days since hypothetical Day 0.", function() {
+        expect(LocalDate.parse("1-1-1").sinceDayZero()).toBe(1);
+        expect(LocalDate.parse("1-2-2").sinceDayZero()).toBe(33);
+        expect(LocalDate.parse("4-3-3").sinceDayZero()).toBe(1158);
     });
 })
 
@@ -88,13 +72,13 @@ describe("Period", function() {
     });
 })
 
-describe("DateCalc", function() {
-    it("can calculate the period between the start and the end dates.",
-        function() {
-            expect(DateCalc.between("0001-01-01",
-                "0001-01-02").toString()).toBe("1 day");
-            expect(DateCalc.between("2000-01-01",
-                "2001-03-02").toString()).toBe("1 year 2 months 1 day");
-        }
-    );
+describe("Utils", function() {
+    it("can check if a given year is a leap year.", function() {
+        expect(Utils.isLeapYear(1)).toBe(false);
+        expect(Utils.isLeapYear(4)).toBe(true);
+        expect(Utils.isLeapYear(100)).toBe(false);
+        expect(Utils.isLeapYear(400)).toBe(true);
+        expect(Utils.isLeapYear(1900)).toBe(false);
+        expect(Utils.isLeapYear(2000)).toBe(true);
+    });
 })
